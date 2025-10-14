0f022641faeb0513c4020105ddc2fcf89b1563a2
Kiiya <a28894@svalero.com>
2025-10-15 00:01:42 +0200

    - Fix VS Code not detecting project solutions other than TetriON

---
+++ TetriON.Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using Netly;
+
+namespace TetriON.Server;
+
+public static class Program {
+
+    private static UDP.Server? _server;
+
+    [STAThread]
+    public static void Main(string[] args) {
+        _server = new UDP.Server();
+        _server.To.Open(new Host("127.0.0.1", 11000));
+
+
+    }
+}
