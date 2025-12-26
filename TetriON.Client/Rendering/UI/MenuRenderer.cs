30cebf568ffa20eb3a4290ca218ea6c9f6ac08ed
Kiiya <a28894@svalero.com>
2025-12-26 22:28:28 +0100

    Create Base Classes for TetriON.Client

---
+++ TetriON.Client/Rendering/UI/MenuRenderer.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TetriON.Client.Rendering.UI
+{
+    public class MenuRenderer
+    {
+        
+    }
+}
---
+++ TetriON.Client/Skin/SkinManager.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TetriON.Client.Skin
+{
+    public class SkinManager
+    {
+        
+    }
+}
